e6f0eeaa06c5cc10209d1091c977d71a87f36711
yasmineamr98 <yasmine_amr2008@hotmail.com>
2024-08-12 17:46:31 +0300

    books schema/ bookslist updated in frontend

--- Server/Schemas/images.schema.ts
+++ Server/Schemas/images.schema.ts
@@ -1,5 +1,5 @@
 import * as mongoose from "mongoose";
-const { MongooseFindByReference } = require('mongoose-find-by-reference');
+const { MongooseFindByReference } = require("mongoose-find-by-reference");
 const imagesSchema = new mongoose.Schema({
   _id: {},
   name: {
@@ -10,14 +10,10 @@ const imagesSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  User: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  Book: { type: mongoose.Schema.Types.ObjectId, ref: 'Book' },
-  Author: { type: mongoose.Schema.Types.ObjectId, ref: 'Author' },
-
+  User: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  Book: { type: mongoose.Schema.Types.ObjectId, ref: "Book" },
+  Author: { type: mongoose.Schema.Types.ObjectId, ref: "Author" },
 });
 imagesSchema.plugin(MongooseFindByReference);
-const ImageModel = mongoose.model('Image', imagesSchema);
-export {
-  ImageModel,
-    imagesSchema 
- };
+const ImageModel = mongoose.model("image", imagesSchema);
+export { ImageModel, imagesSchema };
