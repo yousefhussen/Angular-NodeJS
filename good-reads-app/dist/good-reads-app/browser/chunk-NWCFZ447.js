4f50301f044a8048177c40cfeb01a454a105a8c4
yousefhussen <yousefhussen139@gmail.com>
2024-08-30 21:03:03 +0300

    build

---
+++ good-reads-app/dist/good-reads-app/browser/chunk-NWCFZ447.js
@@ -0,0 +1,1 @@
+import{W as o,fa as r,ic as e}from"./chunk-UXP3UPEQ.js";var n=(()=>{let t=class t{};t.\u0275fac=function(m){return new(m||t)},t.\u0275mod=r({type:t}),t.\u0275inj=o({imports:[e]});let i=t;return i})();var u=(()=>{let t=class t{};t.\u0275fac=function(m){return new(m||t)},t.\u0275mod=r({type:t}),t.\u0275inj=o({imports:[e,n]});let i=t;return i})();export{u as UserModule};
