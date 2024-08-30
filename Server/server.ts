c72952e1b64bec26e486fe07cd1023d62216fa3b
yousefhussen <yousefhussen139@gmail.com>
2024-08-30 15:48:42 +0300

    Update server.ts

--- Server/server.ts
+++ Server/server.ts
@@ -50,7 +50,7 @@ mongoose
 
     // start the Express server
     app.listen(5200, () => {
-      console.log(`Server running at http://localhost:5200...`);
+      console.log(`Server running at ${process.env.ATLAS_URI}...`);
     });
   })
   .catch((error) => console.error(error));
